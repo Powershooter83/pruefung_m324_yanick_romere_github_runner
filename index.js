8dd2e09e07fac424054ccc0e90c07867306ecfb9
p20102 <yanicksilvan.romere@css.ch>
2024-12-12 14:32:33 +0100

    changed index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import chalk from 'chalk';
 
 const myUnusedVariable = 'test'
 const x = 10
-if (x === 10) {
+if (x === 12) {
     console.log(myUnusedVariable);
 }
 console.log(chalk.blue('It works'));
