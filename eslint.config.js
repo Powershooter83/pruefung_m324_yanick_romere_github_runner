d95b7b31ee32745f2b59bd024c73999b2a5ba734
p20102 <yanicksilvan.romere@css.ch>
2024-12-12 14:23:36 +0100

    linter test

--- eslint.config.js
+++ eslint.config.js
@@ -2,19 +2,20 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.mocha,
-      },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.mocha,
+            },
+        },
     },
-  },
-  pluginJs.configs.recommended,
-  {
-    rules: {
-      eqeqeq: ['error', 'always'], // Enforce === and !== over == and !=
-      'no-var': 'error', // Disallow the use of var
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            eqeqeq: ['error', 'always'], // Enforce === and !== over == and !=
+            'no-var': 'error', // Disallow the use of var,
+            'prefer-const': 'error',
+        },
     },
-  },
 ];
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import chalk from 'chalk';
 
+let myUnusedVariable = 'test'
+const x = 10
+if (x == 10) {
+    console.log('x is 10');
+}
+console.log(test)
 console.log(chalk.blue('It works'));
