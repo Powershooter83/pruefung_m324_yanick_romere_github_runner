9c6a8d82eaad8b68106dc86dfb4348387e40f391
p20102 <yanicksilvan.romere@css.ch>
2024-12-12 14:25:28 +0100

    linter fixed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import chalk from 'chalk';
 
-let myUnusedVariable = 'test'
+const myUnusedVariable = 'test'
 const x = 10
-if (x == 10) {
-    console.log('x is 10');
+if (x === 10) {
+    console.log(myUnusedVariable);
 }
 console.log(chalk.blue('It works'));
