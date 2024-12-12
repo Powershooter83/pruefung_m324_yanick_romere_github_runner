c7261ff60332611cc42b89be8d4e18ccdc884e7c
p20102 <yanicksilvan.romere@css.ch>
2024-12-12 14:51:34 +0100

    convert md to html

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import chalk from 'chalk';
 
-const myUnusedVariable = 'test'
-const x = 10
+const myUnusedVariable = 'test';
+const x = 10;
 if (x === 10) {
-    console.log(myUnusedVariable);
+  console.log(myUnusedVariable);
 }
 console.log(chalk.blue('It works'));
